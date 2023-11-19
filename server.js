5154476f62efa7dfa7bbb8ef64b8748c126ef148
Nathan Dona <nathan84dona@gmail.com>
2023-11-18 20:25:29 -0500

    heroku debugging

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ const app = express();
 app.set('port', (process.env.PORT || 5000));
 
 if (process.env.NODE_ENV === 'production'){
-    app.use(express.static('frontend/build'));
+    app.use(express.static('appfrontend/build'));
     app.get('*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'appfrontend', 'build', 'index.html'));
     });
 }
 
